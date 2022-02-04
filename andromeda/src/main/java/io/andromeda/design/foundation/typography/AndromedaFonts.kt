e2fcc1990226ab7f72655254e5be487e2a3f81d6
Adit Lal <aditlal90@gmail.com>
2022-02-04 19:34:35 +0530

    Adds default font family for Andromeda theme

---
+++ andromeda/src/main/java/io/andromeda/design/foundation/typography/AndromedaFonts.kt
@@ -0,0 +1,17 @@
+package io.andromeda.design.foundation.typography
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import io.andromeda.design.R
+
+val AndromedaFonts = FontFamily(
+    Font(R.font.andromeda_black, FontWeight.Black),
+    Font(R.font.andromeda_bold, FontWeight.Bold),
+    Font(R.font.andromeda_extrabold, FontWeight.ExtraBold),
+    Font(R.font.andromeda_light, FontWeight.Light),
+    Font(R.font.andromeda_medium, FontWeight.Medium),
+    Font(R.font.andromeda_regular, FontWeight.W400),
+    Font(R.font.andromeda_semibold, FontWeight.SemiBold),
+    Font(R.font.andromeda_thin, FontWeight.Thin)
+)
