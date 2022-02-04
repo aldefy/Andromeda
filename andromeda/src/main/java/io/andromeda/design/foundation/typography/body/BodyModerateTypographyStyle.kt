70db4924157cd4f0199ca0f6cb1b8ce62cad99cf
Adit Lal <aditlal90@gmail.com>
2022-02-04 19:34:11 +0530

    Adds body typography styles

---
+++ andromeda/src/main/java/io/andromeda/design/foundation/typography/body/BodyModerateTypographyStyle.kt
@@ -0,0 +1,20 @@
+package io.andromeda.design.foundation.typography.body
+
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import io.andromeda.design.foundation.typography.BaseTypography
+
+/**
+ * Body Moderate Large typography style
+ */
+class BodyModerateTypographyStyle(fonts: FontFamily) : BaseTypography {
+    override val fontFamily: FontFamily = fonts
+
+    override val fontSize: TextUnit = 16.sp
+
+    override val fontWeight: FontWeight = FontWeight.W400
+
+    override val lineHeight: TextUnit = 20.sp
+}
