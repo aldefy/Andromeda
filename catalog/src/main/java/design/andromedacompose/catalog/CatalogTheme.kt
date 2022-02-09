75cad0e7bd630bc54619be48e64091c2aec16a3d
Adit Lal <aditlal90@gmail.com>
2022-02-09 16:05:11 +0530

    Adds demo for custom fonts in Catalog app

--- catalog/src/main/java/design/andromedacompose/catalog/CatalogTheme.kt
+++ catalog/src/main/java/design/andromedacompose/catalog/CatalogTheme.kt
@@ -1,6 +1,9 @@
 package design.andromedacompose.catalog
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import design.andromedacompose.AndromedaTheme
 import design.andromedacompose.foundation.colors.defaultDarkColors
 import design.andromedacompose.foundation.colors.defaultLightColors
@@ -12,7 +15,19 @@ fun CatalogTheme(
 ) {
     AndromedaTheme(
         colors = if (isLightTheme) defaultLightColors() else defaultDarkColors(),
+        fontFamily = CatalogAppFonts
     ) {
         content()
     }
 }
+
+val CatalogAppFonts = FontFamily(
+    Font(R.font.catalog_black, FontWeight.Black),
+    Font(R.font.catalog_bold, FontWeight.Bold),
+    Font(R.font.catalog_extrabold, FontWeight.ExtraBold),
+    Font(R.font.catalog_extralight, FontWeight.Light),
+    Font(R.font.catalog_medium, FontWeight.Medium),
+    Font(R.font.catalog_regular, FontWeight.W400),
+    Font(R.font.catalog_semibold, FontWeight.SemiBold),
+    Font(R.font.catalog_thin, FontWeight.Thin)
+)
