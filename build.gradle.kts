b1f91ea30430c926c78984f1679e11b1c259781f
Adit Lal <aditlal90@gmail.com>
2026-03-01 13:15:57 +0530

    build: add KMP/CMP plugin declarations to version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.compose.multiplatform.plugin) apply false
     alias(libs.plugins.dokka) apply false
     alias(libs.plugins.ktlint)
     alias(libs.plugins.nexus.publish)
